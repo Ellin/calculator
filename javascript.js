1f859b6cdb1b204eae2dedb2cc0fd62e0125d296
Ellin <1833921+Ellin@users.noreply.github.com>
2026-01-30 09:47:43 +0000

    Add 'click' event listener to buttons

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const buttonId = e.target.id;
+        alert(`do something with ${buttonId}`);
+    });
+});
+ 
