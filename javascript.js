619ac93a5b4ee741a4934947c6ffe03727c4d88d
Ellin <1833921+Ellin@users.noreply.github.com>
2026-01-30 10:03:34 +0000

    Add basic math functions (e.g. add, subtract)

--- javascript.js
+++ javascript.js
@@ -9,3 +9,34 @@ buttons.forEach(button => {
     });
 });
  
+function add(x, y) {
+    const sum = x + y;
+    return sum;
+}
+
+function subtract(x, y) {
+    const difference = x - y;
+    return difference
+}
+
+function multiply(x, y) {
+    const product = x * y;
+    return product;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        alert(`Can't divide by zero!`);
+        return 'error';
+    }
+    const quotient = x / y;
+    return quotient;
+}
+
+function pow(x, y) {
+    const power = x ** y;
+    return power;
+}
+
+
+
