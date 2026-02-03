059d4a053457a2ed319e7be0c53c1002fb145062
Ellin <1833921+Ellin@users.noreply.github.com>
2026-02-03 13:56:11 +0000

    Implement logic for supporting decimal inputs
    Including updating calculations, display, and backspace functions to support decimal numbers

--- javascript.js
+++ javascript.js
@@ -3,16 +3,20 @@
 let number1 = {
     value: null,
     sign: 1,
+    isDecimal: false,
+    decimalPart: '.', // string
     computedValue: function() {
-        return this.value * this.sign;
+        return Number(this.value + this.decimalPart) * this.sign;
     }
 };
 
 let number2 = {
     value: null,
     sign: 1,
+    isDecimal: false,
+    decimalPart: '.',
     computedValue: function() {
-        return this.value * this.sign;
+        return Number(this.value + this.decimalPart) * this.sign;
     }
 };
 
@@ -71,9 +75,17 @@ buttons.forEach(button => {
             if(result !== null && !operator) resetCalculator();
 
             if (operator) {
-                number2.value = Number((number2.value === null ? '' : number2.value) + buttonText);
+                if (number2.isDecimal) {
+                    number2.decimalPart += buttonText;
+                } else {
+                    number2.value = Number((number2.value === null ? '' : number2.value) + buttonText);
+                }
             } else {
-                number1.value = Number((number1.value === null ? '' : number1.value) + buttonText);
+                if (number1.isDecimal) {
+                    number1.decimalPart += buttonText;
+                } else {
+                    number1.value = Number((number1.value === null ? '' : number1.value) + buttonText);
+                }
             }
             display.innerText = createDisplayString();
             return;
@@ -93,6 +105,16 @@ buttons.forEach(button => {
         }
 
         switch (buttonId) {
+            case 'decimal': {
+                if (operator && !number2.isDecimal) {
+                    number2.isDecimal = true;
+                    number2.value ??= 0;
+                } else if (!number1.isDecimal) {
+                    number1.isDecimal = true;
+                }
+                display.innerText = createDisplayString();
+                break;
+            }
             case 'sign-toggle':
                 if (operator) {
                     number2.sign *= -1;
@@ -131,18 +153,35 @@ function createDisplayString() {
     if (isError) return errorMessage;
 
     const operatorSymbol = operator ? operators[operator].symbol : '';
-    const number1Sign = number1.sign < 0 ? '-' : '';
-    const number1Value = (number1.value !== null) ? number1.value : '';
-    const number2Value = (number2.value !== null) ? number2.value : '';
+    const number1String = createNumberString(number1);
+    const number2String = createNumberString(number2);
 
     if (number2.sign < 0) {
-        return number1Sign + number1Value + operatorSymbol + '(-' + number2Value + ')';
+        return number1String + operatorSymbol + '(' + number2String + ')';
     } else {
-        return number1Sign + number1Value + operatorSymbol + number2Value;
+        return number1String + operatorSymbol + number2String;
     }
 }
 
+function createNumberString(number) {
+    const sign = number.sign < 0 ? '-' : '';
+    const decimalPart = number.isDecimal ? number.decimalPart : '';
+    const intPart = number.value ?? '';
+
+    return sign + intPart + decimalPart;
+}
+
 function removeLastDigit(number) {
+// if decimal, remove from decimal part
+    if (number.isDecimal) {
+        if (number.decimalPart === '.') {
+            number.isDecimal = false;
+        } else {
+            number.decimalPart = number.decimalPart.slice(0, -1);
+        }
+        return;
+    } 
+
     if (number.value === null && number.sign < 0) { // last digit is '-'
         number.sign = 1;
     } else if (number.value >= 0 && number.value < 10) {
@@ -156,8 +195,12 @@ function resetCalculator() {
     display.innerText = '';
     number1.value = null;
     number1.sign = 1;
-    number2.value= null;
+    number1.isDecimal = false;
+    number1.decimalPart = '.';
+    number2.value = null;
     number2.sign = 1;
+    number2.isDecimal = false;
+    number2.decimalPart = '.';
     operator = null;
     result = null;
     isError = false;
@@ -169,8 +212,20 @@ function operate(number1, number2, operatorFn) {
 
 function calculate() {
     result = operate(number1.computedValue(), number2.computedValue(), operators[operator].fn);
+    [number1.value, number1.decimalPart] = String(result).split('.');
+
+    if (!Number.isInteger(result)) {
+        number1.isDecimal = true;
+        number1.decimalPart = '.' + number1.decimalPart; 
+    } else {
+        number1.isDecimal = false;
+        number1.decimalPart = '.';
+    }
     number1.sign = result < 0 ? -1 : 1;
-    number1.value = Math.abs(result);
+    number1.value = Math.abs(number1.value);
+
     number2.value = null;
     number2.sign = 1;
+    number2.isDecimal = false;
+    number2.decimalPart = '.';
 }
